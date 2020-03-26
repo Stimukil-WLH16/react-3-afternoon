193171e6e6b3cddca0a62a88d3d92cd0a38d9888
unknown <jeff_cox76@hotmail.com>
2020-03-26 16:35:46 -0600

    test

--- react-3-afternoon/react-3-afternoon/src/App.js
+++ react-3-afternoon/react-3-afternoon/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import data from './data'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super()
+
+    this.state = {
+      currentIndex: 1
+    }
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <header className="header-box">Home</header> 
+        <body className="main-body-box">
+        <div className="inside-body-box">
+        <div className="name-box"></div>
+          <div className="from-box">From: </div>
+          <div className="job-title-box">Job Title: </div>
+          <div className="employer-box">Employer: </div>
+          <div className="favorite-movies-box">Favorite Movies: </div>
+            <ol className="movie-list">
+              <li></li>
+              <li></li>
+              <li></li>
+            </ol>
+        </div>
+        <div className="tool-bar">
+          <button className="previous-button" >Previous</button>
+            <div className="center-button-group">
+              <button className="edit-button">Edit</button>
+              <button className="delete-button">Delete</button>
+              <button className="new-button">New</button>
+            </div>
+          <button className="next-button" >Next</button>
+        </div>
+        </body>
+      </div>
+    )
+  }
 }
 
 export default App;
